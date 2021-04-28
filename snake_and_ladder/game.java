2fc8d5e51c816b097e7834f1ce3cd2ed5f918f61
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-04-28 23:16:45 +0530

    Added:code with random options

--- snake_and_ladder/game.java
+++ snake_and_ladder/game.java
@@ -2,11 +2,27 @@ package snake_and_ladder;
 
 public class game {
 	public static int player1_position=0;
+	public static int dice1 =(int)(Math.random()*6+1);
 
 	public static void main(String[] args) {
-		int dice1 =(int)(Math.random()*6+1);
+		
 		System.out.println("The number after rolling the dice is: "+ dice1);
-		// TODO Auto-generated method stub
+		int possibility=(int)(Math.random()*10)%3;
+		System.out.println("random possibility "+possibility);
+		switch (possibility) {
+		  case 0:
+		    System.out.println("no play");
+		    break;
+		  case 1:
+		    System.out.println("ladder");
+		    break;
+		  case 2:
+		    System.out.println("snake");
+		    break;
+		}
+		
+		
+		
 
 	}
 
