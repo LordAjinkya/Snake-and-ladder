d869573714ac07143c72bd105097ea6b6a406cdd
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-04-28 20:18:00 +0530

    Added: random no generated

--- snake_and_ladder/game.java
+++ snake_and_ladder/game.java
@@ -1,9 +1,11 @@
 package snake_and_ladder;
 
 public class game {
-	public static int player1_position=0
+	public static int player1_position=0;
 
 	public static void main(String[] args) {
+		int dice1 =(int)(Math.random()*6+1);
+		System.out.println("The number after rolling the dice is: "+ dice1);
 		// TODO Auto-generated method stub
 
 	}
