f47d06d246203a95a8fe4f85a0231990286bc3d6
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-04-28 20:05:58 +0530

    welcome to the snake and ladder game program

---
+++ snake_and_ladder/game.java
@@ -0,0 +1,11 @@
+package snake_and_ladder;
+
+public class game {
+	public static int player1_position=0
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
