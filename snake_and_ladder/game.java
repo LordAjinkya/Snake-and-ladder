d2c84fc5f5853b36b525e9a38c202a38153fccea
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-04-29 11:28:12 +0530

    Refactor: UC3 code

--- snake_and_ladder/game.java
+++ snake_and_ladder/game.java
@@ -3,10 +3,11 @@ package snake_and_ladder;
 public class game {
 	public static int player1_position=0;
 	public static int dice1 =(int)(Math.random()*6+1);
-
-	public static void main(String[] args) {
-		
-		System.out.println("The number after rolling the dice is: "+ dice1);
+	public static int dice_roll_times;
+	public static int roll(int value)
+	{
+		values=value;
+		dice1=dieroll();
 		int possibility=(int)(Math.random()*10)%3;
 		System.out.println("random possibility "+possibility);
 		switch (possibility) {
@@ -20,7 +21,45 @@ public class game {
 		    System.out.println("snake");
 		    break;
 		}
+		return values;
+	}
+	 public static void ladder()
+     {
+         System.out.println("Ladder");
+         if (values+dice1<100)
+         {
+             values=values+dice1;
+
+             if (values<100)
+             {
+                 dice1=dieroll();
+
+                 if (values+dice1<=100)
+                 {
+                     values=values+dice1;
+                 }
+             }
+         }
+     }
+	 public static void snake()
+	 {
+		 if (values>dice1)
+		 {
+			 values =values-dice1;
+		 }
+		 else {
+			 values=0;
+		 }
+	 }
+	
+	
+
+	public static void main(String[] args) {
 		
+		System.out.println("The number after rolling the dice is: "+ dice1);
+		 snake_and_ladder obj1=new snake_and_ladder();
+		player1_position=obj1.play(player1_position);
+        System.out.println("Number Of Die Roll :" +dice_roll_times + "\n" + "Position of Player1 :"+player1_position);
 		
 		
 
