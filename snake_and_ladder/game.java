fd81d61e52ef1018ea7d9bb3fef01daa575d91c2
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-04-29 19:46:57 +0530

    Refactor: corrected program

--- snake_and_ladder/game.java
+++ snake_and_ladder/game.java
@@ -1,68 +1,92 @@
 package snake_and_ladder;
 
 public class game {
-	public static int player1_position=0;
-	public static int dice1 =(int)(Math.random()*6+1);
-	public static int dice_roll_times;
-	public static int roll(int value)
-	{
-		values=value;
-		dice1=dieroll();
-		int possibility=(int)(Math.random()*10)%3;
-		System.out.println("random possibility "+possibility);
-		switch (possibility) {
-		  case 0:
-		    System.out.println("no play");
-		    break;
-		  case 1:
-		    System.out.println("ladder");
-		    break;
-		  case 2:
-		    System.out.println("snake");
-		    break;
-		}
-		return values;
-	}
-	 public static void ladder()
-     {
-         System.out.println("Ladder");
-         if (values+dice1<100)
-         {
-             values=values+dice1;
+	  public static int position_of_player1=0;
+      public static int no_of_die_rolls=0;
+      public static int die_value;
+      public static int values=0;
 
-             if (values<100)
-             {
-                 dice1=dieroll();
 
-                 if (values+dice1<=100)
-                 {
-                     values=values+dice1;
-                 }
-             }
-         }
-     }
-	 public static void snake()
-	 {
-		 if (values>dice1)
-		 {
-			 values =values-dice1;
-		 }
-		 else {
-			 values=0;
-		 }
-	 }
-	
-	
+      
+      public static int dieRoll()
+      {
+              int die_value=(int)(Math.random()*6)+1;
+              return die_value;
+      }
 
-	public static void main(String[] args) {
-		
-		System.out.println("The number after rolling the dice is: "+ dice1);
-		 snake_and_ladder obj1=new snake_and_ladder();
-		player1_position=obj1.play(player1_position);
-        System.out.println("Number Of Die Roll :" +dice_roll_times + "\n" + "Position of Player1 :"+player1_position);
-		
-		
+      public static int play(int value)
+      {
+              values=value;
+              if (values<100)
+              {
+            	  die_value=dieRoll();
+                  System.out.println("DieValue:"+die_value);
+                  int option=(int)(Math.random()*10)%3;
+                  System.out.println("Option:" +option);
+                  switch (option)
+                  {
+                      case 0:
+                          break;
 
-	}
+                      case 1:
+                          ladder();
+                          break;
+
+                      case 2:
+                          snake();
+                          break;
+
+                      
+
+                  }
+                  return values;
+              }
+      }
+
+      public static void ladder()
+      {
+          
+          if (values+die_value<100)
+          {
+              values=values+die_value;
+
+              if (values<100)
+              {
+                  die_value=dieRoll();
+
+                  if (values+die_value<=100)
+                  {
+                      values=values+die_value;
+                  }
+              }
+          }
+      }
+
+      public static void snake()
+      {
+          
+          if (values>die_value && values>0)
+          {
+              values=values-die_value;
+          }
+          else {
+              values=0;
+          }
+      }
+
+      public static void main(String args[])
+      {
+          
+          game playing=new game();
+          while (position_of_player1<100) {
+          position_of_player1=playing.play(position_of_player1);
+          System.out.println("Number Of Die Roll :" +no_of_die_rolls + "\n" + "Position of Player1 :"+position_of_player1);  
+          }
+         
+      }
+          
+
+
 
 }
+
